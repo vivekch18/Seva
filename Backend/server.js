ac18f0dfc9848fa2a92a04c98b0e4ac45791d527
Vivek Raj <vivekchaurasia520@gmail.com>
2025-07-03 02:06:30 +0530

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -71,7 +71,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 
 // Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running at http://localhost:${PORT}`);
+app.get("/", (req, res) => {
+  res.send("Seva backend is running ✅");
 });
+
