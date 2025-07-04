92c9b5368fe38c5caf7574a0b977534a525d3829
Vivek Raj <vivekchaurasia520@gmail.com>
2025-07-04 15:24:43 +0530

    update vite.config

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import dotenv from 'dotenv';
 
-dotenv.config(); // ✅ Load .env file
+const serverUrl = process.env.VITE_SERVER_URL; // 'http://localhost:5000';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': process.env.VITE_SERVER_URL || 'http://localhost:5000', // ✅ Fallback to localhost in dev
+      '/api': serverUrl,
     },
   },
 });
